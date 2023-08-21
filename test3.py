265aa2cacf34a29b477583a2be2702a1900d293f
khalid ibizzi <ibizzikhalid19@gmail.com>
2023-08-21 15:10:06 +0100

    4th try

--- test3.py
+++ test3.py
@@ -7,7 +7,8 @@ def connect_to_database():
             "host": "localhost",
             "user": "root",
             "password": "rootroot",
-            "auth_plugin":'caching_sha2_password'
+            "auth_plugin":'caching_sha2_password',
+            "database" : "db1",
         }
 
         connection = mysql.connector.connect(**db_config)
@@ -17,55 +18,58 @@ def connect_to_database():
     except mysql.connector.Error as err:
         print("Error:", err)
         return None
-
 def fetch_books():
     url = "https://books.toscrape.com/"
     requrl = requests.get(url)
     html = requrl.content
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-    lielements = BeautifulSoup.find_all('li',class_= "col-xs-6 col-sm-4 col-md-3 col-lg-3")
-    booksnames =[]
-
-    divelements= BeautifulSoup.find_all('div',class_='product_price')
-    booksprices=[]
+    lielements = soup.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+    booksnames = []
+    booksprices = []
 
     for l in lielements:
-        tags=l.find_all("h3")
-        for tag in tags :
-            booksnames.append(tags.text.strip())
+        tags = l.find_all("h3")
+        ptags = l.find_all("p",class_="price_color")
 
-    for d in divelements :
-        ptags=d.find_all("p")
-        for ptag in ptags :
-            booksprices.append(ptags.taxt)
+        for tag in tags:
+            booksnames.append(tag.text.strip())
+        for ptag in ptags:
+            price = ptag.text.strip().replace('£', '').replace('$', '')  # Remove currency symbols
+            booksprices.append(float(price))
+
+
 
     connection = connect_to_database()
     if connection:
         try:
-            cursor= connection.cursor()
-            insert_query="INSERT INTO books (title) VALUE (%s)"
-            cursor.execute(insert_query,(booksnames))
+            cursor = connection.cursor()
+            insert_query = "INSERT INTO books (title) VALUES (%s)"
+
+            for book_title in booksnames:
+                cursor.execute(insert_query, (book_title,))
+
             connection.commit()
-            print("stored succesfuly")
-        except mysql.connector.error as err:
-            print("error at data storing")
-        finally :
+            print("Books stored successfully.")
+
+        except mysql.connector.Error as err:
+            print("Error at data storing:", err)
+
+        finally:
             cursor.close()
             connection.close()
 
-
-
-
-
-
     return booksnames
 
-
-
 def main():
+    booksnames,booksprices = fetch_books()
 
-   if __name__ == "__main__":
-    connect_to_database()
+    print("Books:")
+    for book in booksnames:
+        print(book)
+    pront("prices:")
+    for price in booksprices :
+        print("price")
 
-
+if __name__ == "__main__":
+    main()
